0ba54f7678760062bcafebc0a1bf38137aa898d6
Berat Dimen <beratefedimen@hotmail.com>
2024-09-18 21:09:54 +0300

    dialog js yazıldı header kısmı oluşturuldu

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,11 +1,20 @@
+import { useRef, useState } from "react";
 import "./footer.css";
+import Settings from "../Settings";
 
 export default function Footer() {
+  const dialogRef = useRef(false);
+
+  const openModal = () => {
+    dialogRef.current.showModal();
+  };
+
   return (
     <div className="footer">
-      <button>
+      <button onClick={openModal}>
         <img src="./img/settings.png" alt="" />
       </button>
+      <Settings dialogRef={dialogRef} />
     </div>
   );
 }
---
+++ src/components/Settings/index.jsx
@@ -0,0 +1,20 @@
+import "./settings.css";
+
+export default function Settings({ dialogRef }) {
+  const closeModal = () => {
+    dialogRef.current.close();
+  };
+
+  return (
+    <div className="dialogContainer">
+      <dialog ref={(e) => (dialogRef.current = e)}>
+        <div className="dialogHeader">
+          <h3>Settings</h3>
+          <button onClick={closeModal}>
+            <img src="./img/close.png" alt="" />
+          </button>
+        </div>
+      </dialog>
+    </div>
+  );
+}
